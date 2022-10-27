1a33468805fa6321c24eb222b62d95703bfccdfb
Taylor Grinn <grinntaylor@gmail.com>
2022-10-26 21:31:35 -0400

    Added gradle and JavaFX

---
+++ src/main/java/com/cps240/TermProject/Main.java
@@ -0,0 +1,51 @@
+package com.cps240.TermProject;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Pos;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+
+public class Main extends Application 
+{ 
+  
+  @Override
+  public void start(Stage primaryStage) {
+    
+    Label label; 
+    TextField tf;
+    Button button;
+    VBox vbox;
+    Scene scene;
+    
+    tf = new TextField("Text Field!");
+    tf.setMaxWidth(200);
+
+    label = new Label("Please Select Your Game");
+    button = new Button("Connect 4"); 
+
+    button.setOnAction(new EventHandler<ActionEvent>() {
+      @Override public void handle(ActionEvent e) {
+        label.setText(tf.getText());
+      }
+    });
+
+    vbox = new VBox(label, tf, button);
+    vbox.setSpacing(20);
+    vbox.setAlignment(Pos.CENTER);
+    scene = new Scene(vbox, 300, 200);
+    
+    primaryStage.setTitle("Game Launcher");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  } 
+    
+  public static void main(String[] args) {
+    launch(args);
+  }
+} 
---
+++ src/test/java/com/cps240/TermProject/MainTest.java
@@ -0,0 +1,10 @@
+package com.cps240.TermProject;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test void appHasAGreeting() {
+        assertTrue(true);
+    }
+}
