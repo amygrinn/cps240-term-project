e129af4ff9326bd7f3e086781dcf074e19369bf0
Taylor Grinn <grinntaylor@gmail.com>
2022-11-04 12:56:11 -0400

    Trying out testfx

--- src/test/java/com/cps240/TermProject/MainTest.java
+++ src/test/java/com/cps240/TermProject/MainTest.java
@@ -1,12 +1,27 @@
 package com.cps240.TermProject;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
+import org.testfx.assertions.api.Assertions;
+import org.testfx.framework.junit5.ApplicationTest;
 
-class MainTest {
+class MainTest extends ApplicationTest {
+  private Button button;
+
+  @Override
+  public void start(Stage stage) {
+    button = new Button("click me!");
+    button.setOnAction(ev -> button.setText("clicked!"));
+    stage.setScene(new Scene(new StackPane(button), 100, 100));
+    stage.show();
+  }
+
   @Test
-  void appHasAGreeting() {
-    assertTrue(true);
+  void when_button_is_clicked_text_changes() {
+    clickOn(".button");
+    Assertions.assertThat(button).hasText("clicked!");
   }
 }
