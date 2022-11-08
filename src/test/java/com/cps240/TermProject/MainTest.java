67535306b22e124b6cc86ea03d88d740b235182e
Taylor Grinn <grinntaylor@gmail.com>
2022-11-08 14:41:36 -0500

    Updated tests to check for four buttons

--- src/test/java/com/cps240/TermProject/MainTest.java
+++ src/test/java/com/cps240/TermProject/MainTest.java
@@ -1,12 +1,14 @@
 package com.cps240.TermProject;
 
 import javafx.stage.Stage;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.testfx.api.FxAssert;
+import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
-import org.testfx.matcher.control.LabeledMatchers;
+import org.testfx.service.query.NodeQuery;
 
 @ExtendWith(ApplicationExtension.class)
 class MainTest {
@@ -17,7 +19,8 @@ class MainTest {
   }
 
   @Test
-  void has_connect_checkers_button() {
-    FxAssert.verifyThat(".button", LabeledMatchers.hasText("Checkers"));
+  void has_a_button_for_each_game(FxRobot robot) {
+    NodeQuery query = robot.lookup(".button");
+    Assertions.assertThat(query.queryAll().size()).isEqualTo(4);
   }
 }
